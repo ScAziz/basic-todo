d009b3a215d52d227a29efd102686b3193f5fced
Stephen Aziz <stephenaziz.sa@gmail.com>
2021-06-02 23:22:34 +0100

    create new to do item

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const todoInput = document.querySelector('.todo-input');
-const submitTodo = document.querySelector('.submit-todo');
-const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.submit-todo');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addTodoItem);
+
+function addTodoItem(event) {
+  event.preventDefault();
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('todo');
+  const newTodoItem = document.createElement('li');
+  newTodoItem.innerText = 'pls work';
+  newTodoItem.classList.add('todo-item');
+  newDiv.appendChild(newTodoItem);
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-button');
+  newDiv.appendChild(completedButton);
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+  deleteButton.classList.add('delete-button');
+  newDiv.appendChild(deleteButton);
+  todoList.appendChild(newDiv);
+}
